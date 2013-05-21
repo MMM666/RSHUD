bab5aabcbde504570f7f196599236e15dff5d580
MMM666 <areast_666@hotmail.com>
2013-05-21 23:16:37 +0900

    Entityの登録方法を変更。

--- RSHUD-ACV/net/minecraft/src/mod_GRV_RSHUD_ACV.java
+++ RSHUD-ACV/net/minecraft/src/mod_GRV_RSHUD_ACV.java
@@ -47,11 +47,14 @@ public class mod_GRV_RSHUD_ACV extends BaseMod {
 
 	@Override
 	public String getVersion() {
-		return "1.5.2-1";
+		return "1.5.2-2";
 	}
 
 	@Override
 	public void load() {
+		// MMMLibのRevisionチェック
+		MMM_Helper.checkRevision("2");
+		
 		if (MMM_Helper.isClient) {
 			GRV_Client.load(this);
 		}
@@ -69,11 +72,12 @@ public class mod_GRV_RSHUD_ACV extends BaseMod {
 				Character.valueOf('I'), Item.ingotIron,
 				Character.valueOf('R'), Item.redstone
 			});
-			uniqueRECON = MMM_Helper.getNextEntityID(false);
+//			uniqueRECON = MMM_Helper.getNextEntityID(false);
 			classRECON = MMM_Helper.getForgeClass(this, "GRV_EntityRECON");
 			if (classRECON != null) {
-				ModLoader.registerEntityID(classRECON, "RECON", uniqueRECON);
-				ModLoader.addEntityTracker(this, classRECON, uniqueRECON, 80, 10, true);
+				MMM_Helper.registerEntity(classRECON, "RECON", 0, this, 80, 10, false);
+//				ModLoader.registerEntityID(classRECON, "RECON", uniqueRECON);
+//				ModLoader.addEntityTracker(this, classRECON, uniqueRECON, 80, 10, true);
 			}
 		}
 	}
